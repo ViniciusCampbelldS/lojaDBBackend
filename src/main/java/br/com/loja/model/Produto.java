d9b085d762182045f8179c7bcf5ad0596748ea35
vinicius Campbell dos Santos <vinicius.campbell.etm2018.2@gmail.com>
2026-09-21 21:33:57 -0300

    JPA Criando Tabela "produtos"

--- src/main/java/br/com/loja/model/Produto.java
+++ src/main/java/br/com/loja/model/Produto.java
@@ -1,16 +1,30 @@
 package br.com.loja.model;
 import java.math.BigDecimal;
+import jakarta.persistence.*;
 
+//JPA é a ORM, JPA usa Hibernate
+@Entity
+@Table(name="produtos")
 public class Produto {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, length = 100)
 	private String nome;
+	@Column(nullable = false, precision = 10, scale = 2)
 	private BigDecimal preco;
+	@Column(nullable = false)
+	private Integer quantidade;
+	@Column(nullable = false, length = 999)
+	private String descricao;
 	
 	// Construtores:
 	public Produto(){}
-	public Produto(String nome, BigDecimal preco) {
+	public Produto(String nome, BigDecimal preco,  Integer quantidade, String descricao) {
 		this.nome = nome;
 		this.preco = preco;
+		this.quantidade = quantidade;
+		this.descricao = descricao;
 	}
 	
 	// Setters:
@@ -23,6 +37,12 @@ public class Produto {
 	public void setPreco(BigDecimal p){
 		this.preco=p;
 	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	
 	// Getters:
 	public Long getId(){
@@ -34,4 +54,11 @@ public class Produto {
 	public BigDecimal getPreco(){
 		return this.preco;
 	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
 }
+
